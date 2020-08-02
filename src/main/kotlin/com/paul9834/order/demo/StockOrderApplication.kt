f464e819559b0fbd1208a69bca8dc191214781b1
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-08-02 11:40:25 -0500

    Initial commit

---
+++ src/main/kotlin/com/paul9834/order/demo/StockOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.paul9834.order.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<StockOrderApplication>(*args)
+}
